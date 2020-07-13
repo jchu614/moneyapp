bd5211c5c2b87ac076d33b8c6ca11ff2f0aa325a
jchu614 <61766318+jchu614@users.noreply.github.com>
2020-07-13 14:02:47 -0700

    Update Settings.js
    Removed the pre-refactored .js

--- money-app/src/Settings.js
+++ money-app/src/Settings.js
@@ -1,57 +1,6 @@
 import React, {useState} from 'react';
 import './Settings.css';
 
-// class Settings extends Component{
-// 	constructor(props){
-// 		super(props);
-// 		this.state = {inputValue: ''};
-// 		this.handleChange = this.handleChange.bind(this);
-// 		this.handleSubmit = this.handleSubmit.bind(this);
-// 	}
-// 	handleChange(e){
-// 		this.setState({
-// 			inputValue: e.target.value
-// 		})
-// 	}
-// 	handleSubmit(){
-// 		this.props.saveSettings(this.state.inputValue);
-// 	}
-// 	render(){
-// 		const {onClose} = this.props;
-// 		const {resetAmount} = this.props;
-// 		return(
-// 			<div className="menu-container">
-// 			<form className="settings-box">
-// 			  <input 
-// 				  type="number" 
-// 				  value={this.state.inputValue}
-// 				  onChange={this.handleChange}
-// 				  pattern="[0-9]*" 
-// 				  inputMode="numeric" 
-// 				  placeholder="$XX.xx"
-// 				  />
-// 			 <button
-// 				 className="menu-button"
-// 				 type="submit"
-// 				 onClick={this.handleSubmit}
-// 				 >NEW TOTAL VALUE
-// 			</button>
-// 			<button 
-// 				 type="submit"
-// 				 onClick={resetAmount}
-// 				 >RESET AMOUNT
-// 			</button>
-// 			<button 
-// 				 type="submit"
-// 				 onClick={onClose}
-// 				 >CLOSE MENU
-// 			</button>
-// 		  </form>
-// 			</div>
-// 		)
-// 	}
-// }
-
 const Settings = props => {
 	const [amount, setAmount] = useState({value: ""});
 	const [message, setMessage] = useState(null);
